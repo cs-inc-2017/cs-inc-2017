6ba81a5fd14c79f08b79f29a2de052685938fb55
Ronan Pelliard <ronan.pelliard@student.ecp.fr>
2017-11-06 16:41:11 +0100

    add buttons

--- project/project.ino
+++ project/project.ino
@@ -1,23 +1,33 @@
 #include "LCDControl.h"
 #include "Images.h"
 
+#define BTN_BT 8
+#define BTN_AFF 9
+
+int state = 0;
+
 void setup(void)
 {
+  
+  // set pinMode for buttons
+  pinMode(BTN_BT, INPUT);
+  pinMode(BTN_AFF, INPUT);
 
   // initialize and clear screen
   LcdInitialise();
   LcdClear();
-  
+
   // sample display  
   printBitmap(left_roundabout, 0, 0, 38, 4);
   printBitmap(left, 38, 0, 35, 5);
-  printString("Bonjour", 0, 4);
-  
+  printString("Rue Joliot-Curie", 0, 4);
+
 
 }
 
 void loop(void)
 {
-  
+
 }
+
 
